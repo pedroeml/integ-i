a64d734021b260148beffd5eee887317a2bc1ddc
Pedro Elpidio Melha Lemos <pedroemlemos@gmail.com>
2016-11-14 03:29:20 -0200

    It was not possible to finish Stable Marriage Problem in Haskell.

--- StableMarriageProblem/haskell/stable-marriage-problem.hs
+++ StableMarriageProblem/haskell/stable-marriage-problem.hs
@@ -12,7 +12,7 @@ data Person = None | Person {
 	gender :: Gender,
 	preferenceList :: [String],
 	notYetProposed :: [String],
-	matchingPartner :: Person,
+	matchingPartner :: String,
 	free :: Bool
 } deriving Show
 
@@ -30,7 +30,7 @@ getName (Person name _ _ _ _ _) = name
 getGender :: Person -> Gender
 getGender (Person _ gender _ _ _ _) = gender
 
-getMatchingPartner :: Person -> Person
+getMatchingPartner :: Person -> String
 getMatchingPartner (Person _ _ _ _ matchingPartner _) = matchingPartner
 
 isFree :: Person -> Bool
@@ -40,18 +40,23 @@ isNotYetProposedEmpty :: Person -> Bool
 isNotYetProposedEmpty (Person _ _ _ [] _ _) = True
 isNotYetProposedEmpty (Person _ _ _ (x:xs) _ _) = False
 
-popNotYetProposedPerson :: Person -> (Person, String)
-popNotYetProposedPerson (Person n g p (x:xs) m f) = ((Person n g p xs m f), x)
+popNotYetProposedPerson :: Person -> [Person] -> (Person, Person)
+popNotYetProposedPerson (Person n g p (x:xs) m f) l = ((Person n g p xs m f), findName x l)
 
-setMatchingPartner :: Person -> Person -> Person
-setMatchingPartner (Person n g p (x:xs) _ _) None = (Person n g p (x:xs) None True)
-setMatchingPartner (Person n g p (x:xs) _ _) partner = (Person n g p (x:xs) partner False)
+findName :: String -> [Person] -> Person
+findName name [] = None
+findName name (x:xs)
+	| name == getName x = x
+	| otherwise = findName name xs
+
+setMatchingPartner :: Person -> String -> Person
+setMatchingPartner (Person n g p l _ _) partnerName = (Person n g p l partnerName False)
 
 prefersSomeoneOverCurrentMatchingPartner :: Person -> Person -> Bool
-prefersSomeoneOverCurrentMatchingPartner (Person _ _ _ _ None True) _ = True
+prefersSomeoneOverCurrentMatchingPartner (Person _ _ _ _ "" True) _ = True
 prefersSomeoneOverCurrentMatchingPartner (Person _ _ (x:xs) _ m _) someone
-	| someone == m = False 		-- se someone e o partner são os mesmos, então não há razões para comparar
-	| otherwise = prefersAoverB (x:xs) (getName m) (getName someone)
+	| getName someone == m = False 		-- se someone e o partner são os mesmos, então não há razões para comparar
+	| otherwise = prefersAoverB (x:xs) m (getName someone)
 		where
 			prefersAoverB :: [String] -> String -> String -> Bool
 			prefersAoverB [] _ _ = False
@@ -66,28 +71,61 @@ thereIsAnyManFree (x:xs)
 	| isFree x = True
 	| otherwise = thereIsAnyManFree xs
 
-doMatching :: [Person] -> [Person] -> [(Person, Person)]
+{-
+Tentei adaptar o algoritmo presente em Matching.java no código comentado abaixo, mas não foi possível concluir.
+Por conta de Haskell não permitir que os dados sejam modificados, tentei contornar isso criando sucessivamente
+uma nova lista 'men' e 'women' a cada vez que um type class de Person fosse criado a partir de uma modificação
+como troca de partner e pop da notYetPorposedPersons. Esta estratégia é complicada de manter para este problema
+ainda quando é necessário adaptar iteração na lista 'men' dentro de while (thereIsAnyManFree()) usando apenas
+recursão.
+
+doMatching :: [Person] -> [Person] -> ([Person], [Person])
 doMatching [] _ = []
 doMatching _ [] = []
-doMatching (x:xs) (y:ys) = whileThereIsAnyManFree (x:xs) (y:ys)
+doMatching menList womenList = whileThereIsAnyManFree menList womenList
 	where
-		whileThereIsAnyManFree :: [Person] -> [Person] -> [(Person, Person)]
-		whileThereIsAnyManFree menList womenList
-			| thereIsAnyManFree menList
+		whileThereIsAnyManFree :: [Person] -> [Person] -> ([Person], [Person])
+		whileThereIsAnyManFree (x:xs) (y:ys)
+			| thereIsAnyManFree (x:xs) = whileThereIsAnyManFree menList womenList
+			| otherwise = ((x:xs), (y:ys))
+				where
+					(menList, womenList) = foreachMan (x:xs) (y:ys) 
 
+foreachMan :: [Person] -> [Person] -> ([Person], [Person])
+foreachMan [] _ = ([], [])
+foreachMan _ [] = ([], [])
+foreachMan (x:xs) (y:ys)
+	| not (isFree x) = foreachMan xs (y:ys)
+	| not (isNotYetProposedEmpty x) = foreachMan (updateLists m (updateLists p (x:xs))) (updateLists w (y:ys))
+	| otherwise = ([x] ++ foreachMan xs (y:ys), (y:ys))
+		where
+			(m, w, p) = aux (popNotYetProposedPerson x (y:ys))
+				where
+					aux :: (Person, Person) -> [Person] -> (Person, Person, Person) 
+					aux man woman menList
+						| isFree woman = (m, w, None)
+						| prefersSomeoneOverCurrentMatchingPartner woman man = (m, w, p)
+							where
+								m = setMatchingPartner man (getName woman)
+								w = setMatchingPartner woman (getName man)
+								p = setMatchingPartner (findName (getMatchingPartner w) menList) ""
+-}
 
+updateLists :: Person -> [Person] -> [Person]
+updateLists p [] = []
+updateLists None (x:xs) = (x:xs)
+updateLists p (x:xs)
+	| p == x = (p:xs)
+	| otherwise = [x] ++ updateLists p xs
 
-
-
-
-m1 = Person {name = "1", gender = MALE, preferenceList = ["4", "1", "2", "3"], notYetProposed = ["4", "1", "2", "3"], matchingPartner = None, free = True}
-m2 = Person {name = "2", gender = MALE, preferenceList = ["2", "3", "1", "4"], notYetProposed = ["2", "3", "1", "4"], matchingPartner = None, free = True}
-m3 = Person {name = "3", gender = MALE, preferenceList = ["2", "4", "3", "1"], notYetProposed = ["2", "4", "3", "1"], matchingPartner = None, free = True}
-m4 = Person {name = "4", gender = MALE, preferenceList = ["3", "1", "4", "2"], notYetProposed = ["3", "1", "4", "2"], matchingPartner = None, free = True}
-a1 = Person {name = "1", gender = FEMALE, preferenceList = ["4", "1", "3", "2"], notYetProposed = ["4", "1", "3", "2"], matchingPartner = None, free = True}
-a2 = Person {name = "2", gender = FEMALE, preferenceList = ["1", "3", "2", "4"], notYetProposed = ["1", "3", "2", "4"], matchingPartner = None, free = True}
-a3 = Person {name = "3", gender = FEMALE, preferenceList = ["1", "2", "3", "4"], notYetProposed = ["1", "2", "3", "4"], matchingPartner = None, free = True}
-a4 = Person {name = "4", gender = FEMALE, preferenceList = ["4", "1", "2", "3"], notYetProposed = ["4", "1", "2", "3"], matchingPartner = None, free = True}
+m1 = Person {name = "1", gender = MALE, preferenceList = ["4", "1", "2", "3"], notYetProposed = ["4", "1", "2", "3"], matchingPartner = "", free = True}
+m2 = Person {name = "2", gender = MALE, preferenceList = ["2", "3", "1", "4"], notYetProposed = ["2", "3", "1", "4"], matchingPartner = "", free = True}
+m3 = Person {name = "3", gender = MALE, preferenceList = ["2", "4", "3", "1"], notYetProposed = ["2", "4", "3", "1"], matchingPartner = "", free = True}
+m4 = Person {name = "4", gender = MALE, preferenceList = ["3", "1", "4", "2"], notYetProposed = ["3", "1", "4", "2"], matchingPartner = "", free = True}
+a1 = Person {name = "1", gender = FEMALE, preferenceList = ["4", "1", "3", "2"], notYetProposed = ["4", "1", "3", "2"], matchingPartner = "", free = True}
+a2 = Person {name = "2", gender = FEMALE, preferenceList = ["1", "3", "2", "4"], notYetProposed = ["1", "3", "2", "4"], matchingPartner = "", free = True}
+a3 = Person {name = "3", gender = FEMALE, preferenceList = ["1", "2", "3", "4"], notYetProposed = ["1", "2", "3", "4"], matchingPartner = "", free = True}
+a4 = Person {name = "4", gender = FEMALE, preferenceList = ["4", "1", "2", "3"], notYetProposed = ["4", "1", "2", "3"], matchingPartner = "", free = True}
 
 men = [m1, m2, m3, m4]
 women = [a1, a2, a3, a4]
